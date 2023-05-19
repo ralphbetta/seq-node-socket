b1c379ed97cc1a004012e4a9cc6a743cb7feceea
Betta <betta@Bettas-MacBook-Pro.local>
2023-05-19 13:26:08 +0100

    connected to database

---
+++ src/model/database/index.js
@@ -0,0 +1,27 @@
+const enviromentalConfig = require('../../config/db.config');
+const dbConfig = enviromentalConfig.development;
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: 0,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// const Tutorial =  db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+// const User = db.tutorials = require("./user.model.js")(sequelize, Sequelize);
+// const AccessToken = db.tutorials = require("./accessToken.model.js")(sequelize, Sequelize);
+
+module.exports = {db};
--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,49 @@ require('dotenv').config();
 const app = express();
 
 
-class Server{
+class Server {
 
     static boot() {
 
-        
+        // Define CORS options
+        const corsOptions = {
+            origin: "http://localhost:8081", // Specify the allowed origin
+            methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
+            allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
+            credentials: true, // Enable sending cookies across different domains
+            preflightContinue: false, // Disable preflight caching
+            optionsSuccessStatus: 200 // Specify the status code to use for successful OPTIONS requests
+        };
+
+        app.use(cors(corsOptions));
+
+
+        const { db } = require("../model/database/index");
+
+        //Sync Database Models
+        //In development, you may need to drop existing tables and re-sync database. Just use
+        //{ force: true }
+        db.sequelize.sync()
+            .then(() => {
+                console.log("Synced db.");
+            })
+            .catch((err) => {
+                console.log("Failed to sync db: " + err.message);
+            });
+
+
+        // simple route
+        app.get("/", (req, res) => {
+            res.json({ message: "Welcome to bezkoder application." });
+        });
+
+
+        // set port, listen for requests
+        const PORT = process.env.PORT || 8080;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}.`);
+        });
+
 
     }
 
