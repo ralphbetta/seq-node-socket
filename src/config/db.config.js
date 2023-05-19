3890ae8ebd128f8340fd4e7b2864e7d6a276ed42
Betta <betta@Bettas-MacBook-Pro.local>
2023-05-19 13:13:18 +0100

    setup working environment

---
+++ src/config/db.config.js
@@ -0,0 +1,43 @@
+const enviromentalConfig = {
+    development: {
+        HOST: "127.0.0.1",
+        USER: "root",
+        PASSWORD: "",
+        DB: "testdb",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+    test: {
+        HOST: "127.0.0.1",
+        USER: "root",
+        PASSWORD: "",
+        DB: "testdb",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+    production: {
+        HOST: "127.0.0.1",
+        USER: "root",
+        PASSWORD: "xyz",
+        DB: "testdb",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+}
+
+module.exports = enviromentalConfig;
---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+
+
+class Server{
+
+    static boot() {
+
+        
+
+    }
+
+}
+
+module.exports = Server;
