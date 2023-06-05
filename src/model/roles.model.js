1654111e5c6115779a5b0af47903de9f7c82fd1a
ralphbetta <ralphbetta@gmail.com>
2023-06-05 09:43:14 +0100

    clean up role model

--- src/model/roles.model.js
+++ src/model/roles.model.js
@@ -1,24 +1,20 @@
 module.exports = (sequelize, Sequelize) => {
-  const Role = sequelize.define('Role', {
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
+  const Role = sequelize.define('Role',
+    {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
     },
-  },
-    { timestamps: false },
-
-    // {
-
-    //   // Exclude fields from being returned in the model
-    //   defaultScope: {
-    //     attributes: {
-    //       exclude: ['UserRole', 'name',]
-    //     }
-    //   }
-
-    // },
-
+    {
+      timestamps: false,
+      defaultScope: {
+        attributes: {
+          exclude: [''],
+        },
+      },
+    }
   );
 
   return Role;
